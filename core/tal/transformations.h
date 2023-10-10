8537b4afe9cee71f0a0a7ed4c314f1a6e3a6f59a
Thomas Lukasiewicz <thomas.lukasiewicz@mymail.champlain.edu>
2023-10-10 12:47:37 -0400

    minor changes didnt help

--- core/tal/transformations.h
+++ core/tal/transformations.h
@@ -64,7 +64,7 @@ namespace tal {
 		ew::Vec3 rotation = ew::Vec3(0.0f, 0.0f, 0.0f);
 		ew::Vec3 scale = ew::Vec3(1.0f, 1.0f, 1.0f);
 		ew::Mat4 getModelMatrix() const {
-			return Translate(position) * RotateZ(rotation.z) * RotateX(rotation.x) * RotateY(rotation.y) * Scale(scale);
+			return Translate(position) * RotateZ(rotation.z) * RotateX(rotation.x) * RotateY(rotation.y) * Scale(scale) * Identity();
 		}
 	};
 }
