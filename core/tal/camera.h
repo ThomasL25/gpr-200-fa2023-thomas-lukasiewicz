3c0b7df580d4e4fd7889df517c2f0f074de6df93
Thomas Lukasiewicz <thomas.lukasiewicz@mymail.champlain.edu>
2023-10-17 02:34:44 -0400

    camera.h should be settled now i think?

--- core/tal/camera.h
+++ core/tal/camera.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "../ew/ewMath/mat4.h"
 #include "../ew/ewMath/vec3.h"
+#include "../tal/transformations.h"
 
 namespace tal {
 	struct Camera {
@@ -12,7 +13,20 @@ namespace tal {
 		float farPlane; // Far plane distance (+Z)
 		bool orthographic; // Perspective or orthographic?
 		float orthoSize; // Height of the orthographic frustum
-		ew::Mat4 ViewMatrix(); // Word -> View
-		ew::Mat4 ProjectionMatrix(); // View -> Clip
+		// Word -> View
+		ew::Mat4 ViewMatrix() {
+			return LookAt(position, target, Vec3(0, 1, 0));
+		};
+		// View -> Clip
+		ew::Mat4 ProjectionMatrix() {
+			if (orthographic)
+			{
+				return Orthographic(orthoSize, aspectRatio, nearPlane, farPlane);
+			}
+			else
+			{
+				return Perspective(fov, aspectRatio, nearPlane, farPlane);
+			}
+		}
 	};
 }
